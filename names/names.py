364f7c96b7063724caac56939aa18b8d78a10900
Cody Brewer <codybrewerdev@gmail.com>
2020-08-29 10:23:28 -0600

    stretch goal created duplicates using array and intersection method

--- names/names.py
+++ names/names.py
@@ -19,26 +19,28 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-# initialize BST with placeholder node
-bst = BSTNode('placeholder')
+# # initialize BST with placeholder node
+# bst = BSTNode('placeholder')
 
-# for each name in names_1
-for name in names_1:
-    # insert the name into the binary search tree
-    bst.insert(name)
+# # for each name in names_1
+# for name in names_1:
+#     # insert the name into the binary search tree
+#     bst.insert(name)
 
-# for each name in names_2
-for name in names_2:
-    # check if the binary search tree contains the name
-    if bst.contains(name):
-        # if it does appen the name to the duplicates list
-        duplicates.append(name)
-
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+# # for each name in names_2
+# for name in names_2:
+#     # check if the binary search tree contains the name
+#     if bst.contains(name):
+#         # if it does appen the name to the duplicates list
+#         duplicates.append(name)
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+duplicates = set(names_1).intersection(names_2)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
